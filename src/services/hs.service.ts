caf6b483222021988c07e8c8e81a579b4e40b990
Justin <justin.g.robinson@gmail.com>
2023-05-29 09:22:22 +0200

    batch update bug fixed

--- src/services/hs.service.ts
+++ src/services/hs.service.ts
@@ -44,30 +44,32 @@ export class HsService {
     const update: Array<ContactAccountPartial> = [];
     for (let item of items) {
       if (item.Email) {
-        if(new RegExp(/^[\w.+\-]+@[\w\-]+\.[a-z]{2,}$/i).test(item.Email)) {
+        if (new RegExp(/^[\w.+\-]+@[\w\-]+\.[a-z]{2,}$/i).test(item.Email)) {
           const exists = await this.contactExists(item.Email);
           if (exists) {
             update.push(item);
           } else {
             create.push(item);
           }
-        }else{
-          this.logger.error(`Email ${item.Email} is invalid, for item: ${JSON.stringify(item)}`);
+        } else {
+          this.logger.error(
+            `Email ${item.Email} is invalid, for item: ${JSON.stringify(item)}`,
+          );
         }
       } else {
         this.logger.info(`Email is null, for item: ${JSON.stringify(item)}`);
       }
     }
     if (update.length) {
-      await this.batchUpdateCompaniesAndContacts(create);
+      await this.batchUpdateCompaniesAndContacts(update);
     }
     if (create.length) {
       const pairs = this.mapContactAccountPartial(create);
       const settled = await Promise.allSettled(
         pairs.map((pair) => this.createCompanyWithAssociatedContact(pair)),
       );
-      for(let result of settled) {
-        if(result.status === `rejected`) {
+      for (let result of settled) {
+        if (result.status === `rejected`) {
           this.logger.error(`HsService : upsert : ${result.reason}`);
         }
       }
@@ -78,106 +80,119 @@ export class HsService {
     company,
     contact,
   }: ContactCompanyPair) {
-   try {
-     const createCompany = await this.client.crm.companies.basicApi.create({
-       properties: company,
-     });
-     const companyId = createCompany.id;
-     this.logger.info(
-       `HsService : createCompanyWithAssociatedContact : companyId: ${companyId}`,
-     );
-     const createContact = await this.client.crm.contacts.basicApi.create({
-       properties: contact,
-     });
-     const contactId = createContact.id;
-     this.logger.info(
-       `HsService : createCompanyWithAssociatedContact : contactId: ${contactId}`,
-     );
-     this.persistanceService.addSfHsEntry(
-       company.salesforceaccountid,
-       companyId,
-       contactId,
-     );
-     const associationTypes = await this.client.crm.associations.typesApi.getAll(
-       `company`,
-       `contacts`,
-     );
-     const targetType = associationTypes.results.find((types) => {
-       return types.name === `company_to_contact`;
-     });
-     if (!targetType) {
-       throw new Error(
-         `associationType for company_to_contact not found, unable to associate contact with company`,
-       );
-     }
- 
-     const assocInput = new BatchInputPublicAssociation();
-     const ass = new PublicAssociation();
-     ass.type = `company_to_contact`;
-     ass._from = { id: companyId };
-     ass.to = { id: contactId };
-     assocInput.inputs = [ass];
-     const assResult: BatchResponsePublicAssociation =
-       await this.client.crm.associations.batchApi.create(
-         `Companies`,
-         `Contacts`,
-         assocInput,
-       );
-     this.logger.info(
-       `HsService : createCompanyWithAssociatedContact : assResult: ${JSON.stringify(
-         assResult,
-       )}`,
-     );
-   } catch (err: any) {
-     this.logger.error(err)
-     throw err
-   }
+    try {
+      const createCompany = await this.client.crm.companies.basicApi.create({
+        properties: company,
+      });
+      const companyId = createCompany.id;
+      this.logger.info(
+        `HsService : createCompanyWithAssociatedContact : companyId: ${companyId}`,
+      );
+      const createContact = await this.client.crm.contacts.basicApi.create({
+        properties: contact,
+      });
+      const contactId = createContact.id;
+      this.logger.info(
+        `HsService : createCompanyWithAssociatedContact : contactId: ${contactId}`,
+      );
+      this.persistanceService.addSfHsEntry(
+        company.salesforceaccountid,
+        companyId,
+        contactId,
+      );
+      const associationTypes =
+        await this.client.crm.associations.typesApi.getAll(
+          `company`,
+          `contacts`,
+        );
+      const targetType = associationTypes.results.find((types) => {
+        return types.name === `company_to_contact`;
+      });
+      if (!targetType) {
+        throw new Error(
+          `associationType for company_to_contact not found, unable to associate contact with company`,
+        );
+      }
+
+      const assocInput = new BatchInputPublicAssociation();
+      const ass = new PublicAssociation();
+      ass.type = `company_to_contact`;
+      ass._from = { id: companyId };
+      ass.to = { id: contactId };
+      assocInput.inputs = [ass];
+      const assResult: BatchResponsePublicAssociation =
+        await this.client.crm.associations.batchApi.create(
+          `Companies`,
+          `Contacts`,
+          assocInput,
+        );
+      this.logger.info(
+        `HsService : createCompanyWithAssociatedContact : assResult: ${JSON.stringify(
+          assResult,
+        )}`,
+      );
+    } catch (err: any) {
+      this.logger.error(err);
+      throw err;
+    }
   }
 
   private async batchUpdateCompaniesAndContacts(
-    create: Array<ContactAccountPartial>,
+    update: Array<ContactAccountPartial>,
   ) {
-    const batch = this.mapContactAccountPartial(create);
-    const companyBatch: CompanyUpdateBatch = {
-      inputs: batch.map((i) => {
-        return {
-          hs_object_id: this.persistanceService.getHsIds(
+    try {
+      const batch = this.mapContactAccountPartial(update);
+      const companyBatch: CompanyUpdateBatch = {
+        inputs: batch.map((i) => {
+          const id = this.persistanceService.getHsIds(
             i.company.salesforceaccountid,
-          ),
-          ...i.company,
-        };
-      }),
-    };
-    this.logger.info(
-      `HsService : batchUpdateCompaniesAndContacts : companyBatch.inputs.length: ${companyBatch.inputs.length}`,
-    );
-    const companyCreateBatchResult =
-      await this.client.crm.companies.batchApi.update(companyBatch);
-    this.logger.info(
-      `HsService : batchUpdateCompaniesAndContacts : companyCreateBatchResult: ${JSON.stringify(
-        companyCreateBatchResult,
-      )}`,
-    );
-    const contactBatch: ContactUpdateBatch = {
-      inputs: batch.map((i) => {
-        return {
-          hs_object_id: this.persistanceService.getHsIds(
-            i.contact.salesforceaccountid,
-          ),
-          ...i.contact,
-        };
-      }),
-    };
-    this.logger.info(
-      `HsService : batchUpdateCompaniesAndContacts : contactBatch.inputs.length: ${contactBatch.inputs.length}`,
-    );
-    const contactCreateBatchResult =
-      await this.client.crm.contacts.batchApi.update(contactBatch);
-    this.logger.info(
-      `HsService : batchUpdateCompaniesAndContacts : contactCreateBatchResult: ${JSON.stringify(
-        contactCreateBatchResult,
-      )}`,
-    );
+          ).companyHsObjectId;
+          this.logger.debug(
+            `batchUpdateCompaniesAndContacts: company : hs_object_id: ${id} from sfId: ${i.company.salesforceaccountid}`,
+          );
+          return {
+            id,
+            properties: i.company,
+          };
+        }),
+      };
+      this.logger.info(
+        `HsService : batchUpdateCompaniesAndContacts : companyBatch.inputs.length: ${companyBatch.inputs.length}`,
+      );
+      const companyCreateBatchResult =
+        await this.client.crm.companies.batchApi.update(companyBatch);
+      this.logger.info(
+        `HsService : batchUpdateCompaniesAndContacts : companyCreateBatchResult: ${JSON.stringify(
+          companyCreateBatchResult,
+        )}`,
+      );
+      const contactBatch: ContactUpdateBatch = {
+        inputs: batch.map((i) => {
+          const id = this.persistanceService.getHsIds(
+            i.company.salesforceaccountid,
+          ).contactHsObjectId;
+          this.logger.debug(
+            `batchUpdateCompaniesAndContacts: contact : hs_object_id: ${id} from sfId: ${i.contact.salesforceaccountid}`,
+          );
+          return {
+            id,
+            properties: i.contact,
+          };
+        }),
+      };
+      this.logger.info(
+        `HsService : batchUpdateCompaniesAndContacts : contactBatch.inputs.length: ${contactBatch.inputs.length}`,
+      );
+      const contactCreateBatchResult =
+        await this.client.crm.contacts.batchApi.update(contactBatch);
+      this.logger.info(
+        `HsService : batchUpdateCompaniesAndContacts : contactCreateBatchResult: ${JSON.stringify(
+          contactCreateBatchResult,
+        )}`,
+      );
+    } catch (err: any) {
+      this.logger.error(err);
+    }
   }
 
   private mapContactAccountPartial(
@@ -193,6 +208,7 @@ export class HsService {
         address: address,
         website: item.Account.Website,
         salesforceaccountid: item.Account.Id,
+        zip: item.Account.ShippingAddress?.postalCode,
       };
       const contact = {
         email: item.Email,
